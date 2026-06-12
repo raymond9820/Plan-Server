a6f9e52ae5207d388def4dbb649147082e6acadb
Raymond raymond9820 <rashid.r1998@gmail.com>
2026-06-12 13:11:00 +0900

    feat: develop author router ejs page

--- train.js
+++ train.js
@@ -1,1 +1,4 @@
 console.log("train ishga tushdi: we are start,npm run train");
+/*Json format bilan Json object farqi juda oddiy:
+json format: "author" (key)larda qo'shtirnoq boladi.
+json object: author yani stringdan objectga aylantirib beradi.*/
